73af63fd16d4df057b1f80b65ec78b50591afd26
unknown <akshayshinde0596@gmail.com>
2023-12-31 11:19:05 +0530

    added one method

---
+++ src/test/java/test/Akshay.java
@@ -0,0 +1,17 @@
+package test;
+
+
+
+public class Akshay {
+	
+	public void Demo1()
+	{
+       System.out.println("Demo1 method");
+	}
+	
+	public void Demo2()
+	{
+		System.out.println("Demo2 method");
+	}
+
+}
